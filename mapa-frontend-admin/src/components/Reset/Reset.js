fe00d50130fb4a34d5050beaa3a0a7cd3e00befa
Enzo Fica <enzoefica@gmail.com>
2021-11-01 18:40:16 -0300

    modal reset

--- mapa-frontend-admin/src/components/Reset/Reset.js
+++ mapa-frontend-admin/src/components/Reset/Reset.js
@@ -9,6 +9,8 @@ import {
   CardContent,
   Modal,
 } from "@mui/material";
+import "./Reset.scss";
+import Typography from "@mui/material/Typography";
 
 export default function Reset() {
   return (
@@ -19,21 +21,25 @@ export default function Reset() {
           <Card sx={{ minWidth: 275 }}>
             <CardContent>
               <Box
-                component="form"
+                component="span"
                 sx={{
-                  width: 110,
-                  height: 250,
+                  width: 125,
+                  height: 275,
                   "&:hover": {
                     opacity: [0.9, 0.8, 0.7],
                   },
-                  "& .MuiTextField-root": { m: 2, width: "8cm" },
+                  "& .MuiTextField-root": { m: 6, width: "8cm", ml: 1 },
                 }}
                 noValidate
                 autoComplete="off"
               >
-                <h4 style={{ textAlign: "justify" }}>
+                {/* <h4 style={{ textAlign: "justify" }} position={}>
                   Ingresa tu correo electronico para recuperar tu cuenta.
-                </h4>
+                </h4> */}
+
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Ingresá tu correo electrónico para recuperar tu contraseña.
+                </Typography>
 
                 <TextField
                   required
@@ -41,11 +47,9 @@ export default function Reset() {
                   label="Email"
                   defaultValue=""
                 />
-              </Box>
 
-              <Stack direction="row" ml={2}>
-                <Button variant="contained">Enviar</Button>
-              </Stack>
+                <ModalReset />
+              </Box>
             </CardContent>
           </Card>
         </Grid>
@@ -53,3 +57,50 @@ export default function Reset() {
     </Stack>
   );
 }
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxshadow: 24,
+  p: 4,
+};
+
+function ModalReset() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className="style">
+      <Stack direction="row" ml={4}>
+        <Button variant="contained" onClick={handleOpen}>
+          Enviar
+        </Button>
+      </Stack>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        footer={null}
+        width="700px"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Tu correo electronico fue enviado con éxito.
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Revise su correo electronico para recuperar su contraseña.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+//Agregar error cuando el mail sea incorrecto
