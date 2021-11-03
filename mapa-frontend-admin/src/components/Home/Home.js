2a614f0ee294f2b705ba85e3c3d6d828469b488b
Facu76 <callefacundo@gmail.com>
2021-11-03 20:38:04 -0300

    Tabs con opciones en home

--- mapa-frontend-admin/src/components/Home/Home.js
+++ mapa-frontend-admin/src/components/Home/Home.js
@@ -1,10 +1,43 @@
 import * as React from "react";
-import imageexample from '../../assets/img/imageexample.png'
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import {Grid, Stack} from "@mui/material";
 
 export default function Home() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <img src={imageexample} alt=""/>
-    </div>
+    <Grid container item spacing={1} backgroundColor="lightblue" >
+    <Grid item xs={3}></Grid>
+    <Grid item xs={6}>
+    <Box sx={{ width: '100%',  backgroundColor:"lightblue", textAlign:"center"}}>
+      <Tabs value={value} onChange={handleChange} aria-label="nav tabs example" >
+        <LinkTab label="Usuario" href="/drafts" / >
+        <LinkTab label="Categorias" href="/trash" />
+        <LinkTab label="Lugares" href="/spam" />
+        <LinkTab label="caracteristicas" href="/spam" />
+        <LinkTab label="estadisticas" href="/spam" />
+      </Tabs>
+    </Box>
+    </Grid>
+    </Grid>
   );
 }
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
