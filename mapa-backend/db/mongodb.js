eeefd9d209fc49dd9c7f26e1bb61228b0609d1ff
Enzo <enzoefica@gmail.com>
2022-04-20 17:19:03 -0300

    Volviendo

--- mapa-backend/db/mongodb.js
+++ mapa-backend/db/mongodb.js
@@ -2,20 +2,29 @@ const mongoose = require('mongoose')
 
 mongoose.connection.on('open', () => console.log('db connected'))
 
-async function connectDb () {
-  
-  const connection = await mongoose.connect(process.env.MONGO_URI, 
-  { useNewUrlParser: true 
-    
-  });
-
-  const url = `${connection.connection.host}:${connection.connection.port}`
-  console.log(`MongoDb Conectado en: ${url}`)
+async function connectDb ({ host, port, dbName }) {
+  const uri = `mongodb://${host}:${port}/${dbName}`
+  await mongoose.connect(uri, { useNewUrlParser: true })
 }
+
+// async function connectDb () {
+//   try{
+//   const connection = await mongoose.connect(process.env.MONGO_URI, 
+//   { useNewUrlParser: true, 
+//     useUnifiedTopology: true
+//   });
+
+//   const url = `${connection.connection.host}:${connection.connection.port}`
+//   console.log(`MongoDb Conectado en: ${url}`)
+//   }catch(error){
+//     console.log(`error:${error.message}`);
+//     process.exit(1);
+//   }
+// }
 
 module.exports = connectDb
 
-// async function connectDb ({ host, port, dbName }) {
-//   const uri = `mongodb://${host}:${port}/${dbName}`
-//   await mongoose.connect(uri, { useNewUrlParser: true })
-// }
+ async function connectDb ({ host, port, dbName }) {
+   const uri = `mongodb://${host}:${port}/${dbName}`
+   await mongoose.connect(uri, { useNewUrlParser: true })
+}
